1fde3ce572c4ba87a860cfea4772a24cad0a25af
Nilton Vasques <nilton.vasques@gmail.com>
2016-06-01 02:47:35 -0300

    Clean image detect paper and create a mask

--- main.cpp
+++ main.cpp
@@ -11,23 +11,69 @@
 using namespace cv;
 using namespace std;
 
+Mat img;
+int thresh = 100;
+int max_thresh = 255;
+
+void thresh_callback(int, void*);
+void clean_callback(int, void*);
+
+int range = 200; 
+
+#define MAX_WIDTH 500.0
 
 int main(int argc, char* argv[]){
 
-  Mat img = imread("example.jpg",0);
+  const char* filename = argc >= 2 ? argv[1] : "example.jpg";
+  img = imread(filename, 0);
   //imshow("original", img);
+  Size new_size(MAX_WIDTH, (MAX_WIDTH/img.size().width) * img.size().height);
+  resize(img, img, new_size);
 
-  cleanup_image(img, img);
+  //vector<vector<Point> > squares;
+  //find_squares(img, squares);
+
+  // Apply Histogram Equalization
+//  equalizeHist( img, img );
+  //imshow("original", img);
+  //cleanup_image(img, img);
 
   //imshow("bitwise", img);
+  // Create Window
+  char* source_window = "Source";
+  namedWindow( source_window, CV_WINDOW_AUTOSIZE );
+  imshow( source_window, img );
+  createTrackbar( " Canny thresh:", "Source", &thresh, max_thresh, thresh_callback );
+  thresh_callback( 0, 0 );
+  //createTrackbar( " Canny thresh:", "Source", &range, 255, clean_callback );
+  //clean_callback( 0, 0 );
+  ////imshow("original", img);
 
-  //imshow("cvtColor", img3);
+  waitKey();
 
-  vector<Vec4i> lines = search_lines(img); 
+  return 0;
+}
+
+void clean_callback(int, void*){
+  if(range % 2 == 0) range++;
+  cv::Size size(3,3);  
+  Mat dst;
+  cv::GaussianBlur(img,dst,size,0);  
+  //imshow("blur", img);
+  //imshow("thresh", img);
+  cv::bitwise_not(dst, dst);    
+  threshold( dst, dst, range, 255,0 );
+  //Canny(dst, dst, thresh, thresh * 2, 3);
+  //adaptiveThreshold(dst, dst,255,CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY,75,10);  
+  imshow("Source", dst);
+
+  ///Mat img3;
+  ///cvtColor(drawing,img3, CV_RGB2GRAY);  
+  vector<Vec4i> lines = search_lines(dst); 
 
   // DEBUG PURPOSE
   cv::Mat img2;  
-  cvtColor(img,img2, CV_GRAY2RGB);  
+  cvtColor(dst,img2, CV_GRAY2RGB);  
 
   for(int i = 0; i < lines.size(); i++)
     line(img2, Point(lines[i][0], lines[i][1]), Point(lines[i][2],
@@ -35,12 +81,12 @@ int main(int argc, char* argv[]){
   //imshow("lines",img2);
 
   //Find corners
-  vector<cv::Point2f> corners = find_corners(img, lines);
+  vector<cv::Point2f> corners = find_corners(img2, lines);
 
   //DEBUG CORNERS
   for(int i = 0; i < corners.size(); i++)
     circle( img2, corners[i], 3, Scalar(0,255,0), -1, 8, 0 );
-  //imshow("corners",img2);
+  imshow("corners",img2);
 
   // Get mass center  
   cv::Point2f center = compute_mass_center(corners);
@@ -48,8 +94,8 @@ int main(int argc, char* argv[]){
     center += corners[i];  
   center *= (1. / corners.size()); 
 
-  //circle( img2, center, 3, Scalar(255,255,0), -1, 8, 0 );
-  //imshow("mass",img2);
+  circle( img2, center, 3, Scalar(255,255,0), -1, 8, 0 );
+  imshow("mass",img2);
 
   sortCorners(corners, center); 
 
@@ -63,29 +109,105 @@ int main(int argc, char* argv[]){
   quad_pts.push_back(cv::Point2f(quad.cols, quad.rows));  
   quad_pts.push_back(cv::Point2f(0, quad.rows));  
 
-  cv::Mat img3;  
-  cvtColor(img,img3, CV_GRAY2RGB); 
+  rectangle(img, r, Scalar(255, 0, 255));
+  imshow("rect",img);
+}
 
-  fix_perspective(img3, quad, corners, quad_pts);
+void thresh_callback(int, void*){
+  
+  vector<vector<Point> > contours;
+  vector<vector<Point> > biggest;
+  vector<Vec4i> hierarchy;
+  Mat edges;
+  RNG rng(12345);
+  Canny(img, edges, thresh, thresh * 2, 3);
+  // Find contours
+  findContours( edges, contours, hierarchy, CV_RETR_EXTERNAL,
+      CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
 
-  //imshow("transform",quad);
+  sort(contours.begin(), contours.end(), compareContourAreas);
+  biggest.pb(contours[contours.size()-1]);
+  /// Draw contours
+  Mat drawing = Mat::zeros( edges.size(), CV_8UC3 );
+  Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+  //drawContours( edges, contours, -1, color, 2, 8, hierarchy, 0, Point() );
+  drawContours( drawing, biggest, -1, color, 2 );
 
-  vector<Vec3f> circles = find_circles(quad);
-  for( size_t i = 0; i < circles.size(); i++ ){  
-    Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-    circle( quad, center, 3, Scalar(0,255,0), -1, 8, 0 );
-  }
+  // Show in a window
+  imshow( "Source", drawing );
+
+  // you could also reuse img1 here
+  Mat mask = Mat::zeros(img.rows, img.cols, CV_8UC1);
+
+  // CV_FILLED fills the connected components found
+  drawContours(mask, biggest, -1, Scalar(255), CV_FILLED);
+  imshow("Mask", mask);
+
+  /////imshow("cvtColor", img3);
+
+  /////Mat img3;
+  /////cvtColor(drawing,img3, CV_RGB2GRAY);  
+  //vector<Vec4i> lines = search_lines(edges); 
+
+  //// DEBUG PURPOSE
+  //cv::Mat img2;  
+  //cvtColor(img,img2, CV_GRAY2RGB);  
+
+  //for(int i = 0; i < lines.size(); i++)
+  //  line(img2, Point(lines[i][0], lines[i][1]), Point(lines[i][2],
+  //        lines[i][3]), Scalar(0,0,255), 3, CV_AA);
+  //imshow("lines",img2);
+
+  ////Find corners
+  //vector<cv::Point2f> corners = find_corners(img, lines);
+
+  ////DEBUG CORNERS
+  //for(int i = 0; i < corners.size(); i++)
+  //  circle( img2, corners[i], 3, Scalar(0,255,0), -1, 8, 0 );
+  ////imshow("corners",img2);
+
+  //// Get mass center  
+  //cv::Point2f center = compute_mass_center(corners);
+  //for (int i = 0; i < corners.size(); i++)  
+  //  center += corners[i];  
+  //center *= (1. / corners.size()); 
+
+  ////circle( img2, center, 3, Scalar(255,255,0), -1, 8, 0 );
+  ////imshow("mass",img2);
+
+  //sortCorners(corners, center); 
+
+  //Rect r = boundingRect(corners); 
+  //cout << r << endl;
+  //cv::Mat quad = cv::Mat::zeros(r.height, r.width, CV_8UC3);  
+  //// Corners of the destination image  
+  //std::vector<cv::Point2f> quad_pts;  
+  //quad_pts.push_back(cv::Point2f(0, 0));  
+  //quad_pts.push_back(cv::Point2f(quad.cols, 0));  
+  //quad_pts.push_back(cv::Point2f(quad.cols, quad.rows));  
+  //quad_pts.push_back(cv::Point2f(0, quad.rows));  
+
+  //cv::Mat img3;  
+  //cvtColor(img,img3, CV_GRAY2RGB); 
+
+  //fix_perspective(img3, quad, corners, quad_pts);
+
+  ////imshow("transform",quad);
+
+  //vector<Vec3f> circles = find_circles(quad);
+  //for( size_t i = 0; i < circles.size(); i++ ){  
+  //  Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+  //  circle( quad, center, 3, Scalar(0,255,0), -1, 8, 0 );
+  //}
+  ////imshow("circles",quad);
+
+  ////waitKey();
+
+  //vector<int> markers = find_markers(quad, circles);
+  //for(int i = 0; i < markers.size(); i++){
+  //  printf("%d: %c\n", i+1, 'A'+markers[i]);  
+  //}
+
+  ////// circle outline*/  
   //imshow("circles",quad);
-
-  //waitKey();
-
-  vector<int> markers = find_markers(quad, circles);
-  for(int i = 0; i < markers.size(); i++){
-    printf("%d: %c\n", i+1, 'A'+markers[i]);  
-  }
-
-  //// circle outline*/  
-  imshow("circles",quad);
-  waitKey();
-  return 0;
 }
--- omr.hpp
+++ omr.hpp
@@ -79,7 +79,7 @@ static inline void cleanup_image(cv::Mat &src, cv::Mat &dst){
 static inline vector<Vec4i> search_lines(cv::Mat &src){
   vector<Vec4i> lines;  
 
-  HoughLinesP(src, lines, 1, CV_PI/180, 80, 400, 10);  
+  HoughLinesP(src, lines, 1, CV_PI/180, 80, 30, 10);  
   for( size_t i = 0; i < lines.size(); i++ )  
   {  
     Vec4i l = lines[i];  
@@ -203,4 +203,85 @@ static inline vector<int> find_markers(cv::Mat &src, vector<Vec3f> circles){
   return markers;
 }
 
+static inline double angle( cv::Point pt1, cv::Point pt2, cv::Point pt0 ) {
+    double dx1 = pt1.x - pt0.x;
+    double dy1 = pt1.y - pt0.y;
+    double dx2 = pt2.x - pt0.x;
+    double dy2 = pt2.y - pt0.y;
+    return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+}
+
+static inline void find_squares(Mat& image, vector<vector<Point> >& squares)
+{
+  // blur will enhance edge detection
+  Mat blurred(image);
+  medianBlur(image, blurred, 9);
+
+  Mat gray0(blurred.size(), CV_8U), gray;
+  vector<vector<Point> > contours;
+
+  // find squares in every color plane of the image
+  for (int c = 0; c < 3; c++)
+  {
+    int ch[] = {c, 0};
+    mixChannels(&blurred, 1, &gray0, 1, ch, 1);
+
+    // try several threshold levels
+    const int threshold_level = 2;
+    for (int l = 0; l < threshold_level; l++)
+    {
+      // Use Canny instead of zero threshold level!
+      // Canny helps to catch squares with gradient shading
+      if (l == 0)
+      {
+        Canny(gray0, gray, 10, 20, 3); // 
+
+        // Dilate helps to remove potential holes between edge segments
+        dilate(gray, gray, Mat(), Point(-1,-1));
+      }
+      else
+      {
+        gray = gray0 >= (l+1) * 255 / threshold_level;
+      }
+
+      // Find contours and store them in a list
+      findContours(gray, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+
+      // Test contours
+      vector<Point> approx;
+      for (size_t i = 0; i < contours.size(); i++)
+      {
+        // approximate contour with accuracy proportional
+        // to the contour perimeter
+        approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true)*0.02, true);
+
+        // Note: absolute value of an area is used because
+        // area may be positive or negative - in accordance with the
+        // contour orientation
+        if (approx.size() == 4 &&
+            fabs(contourArea(Mat(approx))) > 1000 &&
+            isContourConvex(Mat(approx)))
+        {
+          double maxCosine = 0;
+
+          for (int j = 2; j < 5; j++)
+          {
+            double cosine = fabs(angle(approx[j%4], approx[j-2], approx[j-1]));
+            maxCosine = MAX(maxCosine, cosine);
+          }
+
+          if (maxCosine < 0.3)
+            squares.push_back(approx);
+        }
+      }
+    }
+  }
+}
+
+static inline bool compareContourAreas ( std::vector<cv::Point> contour1,
+    std::vector<cv::Point> contour2 ) {
+    double i = fabs( contourArea(cv::Mat(contour1)) );
+    double j = fabs( contourArea(cv::Mat(contour2)) );
+    return ( i < j );
+}
 #endif
